1214e432840296087d9a8c346cb0a4763b579c94
James O'Gorman <james@netinertia.co.uk>
2015-03-14 15:28:11 +0000

    Move server config types to their own file.

---
+++ cmd/server/config.go
@@ -0,0 +1,38 @@
+package main
+
+type Config struct {
+	Main struct {
+		Title      string
+		Maintainer string
+		Listen     string
+	}
+	DB      DB
+	Probes  map[string]Probe
+	Workers map[string]Worker
+	Targets map[string]TargetGroup
+}
+
+type DB struct {
+	Host     string
+	Port     int
+	Database string
+	Username string
+	Password string
+}
+
+type Probe struct {
+	Interval int
+}
+
+type Worker struct {
+	Hostname string
+	Display  string
+}
+
+type TargetGroup map[string]Target
+
+type Target struct {
+	Title string
+	Probe string
+	Host  string
+}
--- cmd/server/netsmog.go
+++ cmd/server/netsmog.go
@@ -19,42 +19,6 @@ import (
 //  * UI for viewing graphs
 //  * worker API (fetch configs, post results)
 
-type Config struct {
-	Main struct {
-		Title      string
-		Maintainer string
-	}
-	DB      DB
-	Probes  map[string]Probe
-	Workers map[string]Worker
-	Targets map[string]TargetGroup
-}
-
-type DB struct {
-	Host     string
-	Port     int
-	Database string
-	Username string
-	Password string
-}
-
-type Probe struct {
-	Interval int
-}
-
-type Worker struct {
-	Hostname string
-	Display  string
-}
-
-type TargetGroup map[string]Target
-
-type Target struct {
-	Title string
-	Probe string
-	Host  string
-}
-
 func main() {
 	var confFile = flag.String("config", "config.toml", "config file, in TOML format")
 	flag.Parse()
